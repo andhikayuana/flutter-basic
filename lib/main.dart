fc997e36dad854a5eb13f97115f1dfdc1e93ad8f
andhikayuana <andhikayuana@gmail.com>
2019-10-17 19:36:48 +0700

    drawer section

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,38 @@ class FlutterBasicApp extends StatelessWidget {
     return MaterialApp(
       title: "Flutter Playground",
       home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Playground"),
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              Text("1. Basic"),
+              Divider(),
+              Text("2. Card Widget"),
+              Divider(),
+              Text("3. Drawer Widget"),
+              Divider(),
+              Text("4. BottomNavigationBar"),
+              Divider(),
+              Text("5. TabBar Widget"),
+              Divider(),
+              Text("6. Stepper Widget"),
+              Divider(),
+              Text("7. Form Widget"),
+              Divider(),
+              Text("8. Checkbox, Switch, Slider, & Radio"),
+              Divider(),
+              Text("9. DateTime Picker"),
+              Divider(),
+              Text("10. Simple Dialog"),
+              Divider(),
+              Text("11. SnackBar Widget"),
+              Divider(),
+              Text("12. BottomSheet")
+            ],
+          ),
+        ),
         body: Center(
           child: Row(
             children: <Widget>[
