ac2b5cee64268bb6da84f7eb5f1c554e9d7f270a
andhikayuana <andhikayuana@gmail.com>
2019-10-17 20:52:37 +0700

    update drawer

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,21 @@ import 'package:flutter/material.dart';
 void main() => runApp(FlutterBasicApp());
 
 class FlutterBasicApp extends StatelessWidget {
+  final menuItems = [
+    "1. Basic",
+    "2. Card Widget",
+    "3. Drawer Widget",
+    "4. BottomNavigationBar",
+    "5. TabBar Widget",
+    "6. Stepper Widget",
+    "7. Form Widget",
+    "8. Checkbox, Switch, Slider, & Radio",
+    "9. DateTime Picker",
+    "10. Simple Dialog",
+    "11. SnackBar Widget",
+    "12. BottomSheet"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,42 +26,7 @@ class FlutterBasicApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Flutter Playground"),
         ),
-        drawer: Drawer(
-          child: Column(
-            children: <Widget>[
-              DrawerHeader(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[Text("Jarjit Singh")],
-                ),
-                decoration: BoxDecoration(color: Colors.green),
-              ),
-              Text("1. Basic"),
-              Divider(),
-              Text("2. Card Widget"),
-              Divider(),
-              Text("3. Drawer Widget"),
-              Divider(),
-              Text("4. BottomNavigationBar"),
-              Divider(),
-              Text("5. TabBar Widget"),
-              Divider(),
-              Text("6. Stepper Widget"),
-              Divider(),
-              Text("7. Form Widget"),
-              Divider(),
-              Text("8. Checkbox, Switch, Slider, & Radio"),
-              Divider(),
-              Text("9. DateTime Picker"),
-              Divider(),
-              Text("10. Simple Dialog"),
-              Divider(),
-              Text("11. SnackBar Widget"),
-              Divider(),
-              Text("12. BottomSheet")
-            ],
-          ),
-        ),
+        drawer: createDrawer(context),
         body: Center(
           child: Row(
             children: <Widget>[
@@ -58,5 +38,32 @@ class FlutterBasicApp extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget createDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+          padding: EdgeInsets.zero, children: createDrawerChildren(context)),
+    );
+  }
+
+  List<Widget> createDrawerChildren(BuildContext context) {
+    var childs = <Widget>[];
+    childs.add(
+      DrawerHeader(
+          child: Text("Jarjit Singh"),
+          decoration: BoxDecoration(color: Colors.blue)),
+    );
+
+    for (var label in menuItems) {
+      childs.add(ListTile(
+        title: Text(label),
+        onTap: () {
+          print(label);
+        },
+      ));
+    }
+
+    return childs;
   }
 }
