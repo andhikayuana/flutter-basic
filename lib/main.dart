97afccdf1f856184fd226059a5a775ac3903657e
andhikayuana <andhikayuana@gmail.com>
2019-10-17 21:32:29 +0700

    refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/ui/BasicScreen.dart';
+import 'data/Menu.dart' show menuItems;
+import 'util/DrawerUtil.dart' show createDrawer;
 
 void main() => runApp(FlutterBasicApp());
 
 class FlutterBasicApp extends StatelessWidget {
-  final menuItems = [
-    "1. Basic",
-    "2. Card Widget",
-    "3. Drawer Widget",
-    "4. BottomNavigationBar",
-    "5. TabBar Widget",
-    "6. Stepper Widget",
-    "7. Form Widget",
-    "8. Checkbox, Switch, Slider, & Radio",
-    "9. DateTime Picker",
-    "10. Simple Dialog",
-    "11. SnackBar Widget",
-    "12. BottomSheet"
-  ];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter Playground",
+      initialRoute: "/basic",
+      routes: {"/basic": (context) => BasicScreen()},
       home: Scaffold(
         appBar: AppBar(
           title: Text("Flutter Playground"),
         ),
-        drawer: createDrawer(context),
+        drawer: createDrawer(context, menuItems),
         body: Center(
           child: Row(
             children: <Widget>[
@@ -38,32 +28,5 @@ class FlutterBasicApp extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  Widget createDrawer(BuildContext context) {
-    return Drawer(
-      child: ListView(
-          padding: EdgeInsets.zero, children: createDrawerChildren(context)),
-    );
-  }
-
-  List<Widget> createDrawerChildren(BuildContext context) {
-    var childs = <Widget>[];
-    childs.add(
-      DrawerHeader(
-          child: Text("Jarjit Singh"),
-          decoration: BoxDecoration(color: Colors.blue)),
-    );
-
-    for (var label in menuItems) {
-      childs.add(ListTile(
-        title: Text(label),
-        onTap: () {
-          print(label);
-        },
-      ));
-    }
-
-    return childs;
   }
 }
